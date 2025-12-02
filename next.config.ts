f5d077f423bb9d06b58f8d5fbdf9faea28ad0fed
nkinki <nkinki2014@gmail.com>
2025-12-02 09:40:41 +0100

    Fix MetaMask SDK error - add async-storage to webpack externals

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     reactStrictMode: true,
     webpack: (config) => {
-        config.externals.push("pino-pretty", "lokijs", "encoding");
+        config.externals.push("pino-pretty", "lokijs", "encoding", "@react-native-async-storage/async-storage");
         return config;
     },
 };
